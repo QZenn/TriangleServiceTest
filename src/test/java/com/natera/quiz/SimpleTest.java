00d70a1db17b6ac545c4c29934479c3afd6148bb
Evgeny Pogodin <evgeny.pogodin@gmail.com>
2020-04-26 12:29:29 +0300

    Added log4j2

--- src/test/java/com/natera/quiz/SimpleTest.java
+++ src/test/java/com/natera/quiz/SimpleTest.java
@@ -3,6 +3,8 @@ package com.natera.quiz;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -12,10 +14,16 @@ public class SimpleTest {
 
     String token;
 
+    private static Logger LOGGER = null;
+
     @BeforeClass
     public void configure() {
         RestAssured.baseURI = "https://qa-quiz.natera.com/";
         token = "4a2cb0d2-058e-43aa-a031-0431dce58095";
+
+        System.setProperty("log4j.configurationFile","log4j2.xml");
+        LOGGER = LogManager.getLogger(this.getClass().getName());
+        LOGGER.info("Configuration completed. Start Test Run...");
     }
 
     private RequestSpecification quizRequest() {
